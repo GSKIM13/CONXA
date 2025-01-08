319197b2ce4da8d15ebb91d2cdd139359eb40345
GSKIM13 <129475264+GSKIM13@users.noreply.github.com>
2025-01-08 23:00:53 +0900

    .

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -1,7 +1,8 @@
 from .convnext_v2_case8_bn import ConvNeXt_V2_CASE8_BN
+from .convnext_v2_case8 import ConvNeXt_V2_CASE8
 
 
 
 __all__ = [
-    'ConvNeXt_V2_CASE8_BN'
+    'ConvNeXt_V2_CASE8_BN','ConvNeXt_V2_CASE8'
 ]
