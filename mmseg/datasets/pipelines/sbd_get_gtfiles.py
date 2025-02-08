b8ad4bd0e0c025d0e57a3c390774e657d595ddeb
GSKIM13 <129475264+GSKIM13@users.noreply.github.com>
2025-02-08 20:08:04 +0900

    .

--- mmseg/datasets/pipelines/sbd_get_gtfiles.py
+++ mmseg/datasets/pipelines/sbd_get_gtfiles.py
@@ -14,8 +14,8 @@ def sparse_to_dense(sparse_array):
     
 def load_segmentation_from_mat(mat_file):
     mat_data = scipy.io.loadmat(mat_file)
-    gt_cls = mat_data['GTinst']
-    boundaries = gt_cls['Boundaries'][0, 0] 
+    gt_inst = mat_data['labelEdge']
+    boundaries = gt_inst
     dense_boundaries = sparse_to_dense(boundaries)
     return dense_boundaries
     
