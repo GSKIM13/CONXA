9d9b841abd464ae0a8fd855ddc284933944f94fc
GSKIM13 <129475264+GSKIM13@users.noreply.github.com>
2025-02-08 11:22:46 +0900

    .

--- mmseg/core/evaluation/__init__.py
+++ mmseg/core/evaluation/__init__.py
@@ -1,7 +1,7 @@
 from .class_names import get_classes, get_palette
-from .eval_hooks import DistEvalHook, EvalHook
+from .eval_hooks import DistEvalHook, EvalHook, DistEvalHook_SBD, DistEvalHook_CITY
 from .mean_iou import mean_iou
 
 __all__ = [
-    'EvalHook', 'DistEvalHook', 'mean_iou', 'get_classes', 'get_palette'
+    'EvalHook', 'DistEvalHook', 'mean_iou', 'get_classes', 'get_palette','DistEvalHook_SBD','DistEvalHook_CITY'
 ]
